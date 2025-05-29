354e8dcb212214eaeb53292bf5b047d78bb2d808
nicolasjml01 <nicolasml99@usal.es>
2025-05-29 10:55:12 +0200

    Correccion de carpetas

---
+++ Semana 12_RepasoFinal/MatricesYListas/MatricesyListas.h
@@ -0,0 +1,24 @@
+#ifndef MATRICES_Y_LISTAS_H
+#define MATRICES_Y_LISTAS_H
+
+#define COLS    25
+
+typedef float * floatRef;
+typedef int * intRef;
+typedef char * charRef;
+
+typedef struct matInt {
+	int **m;
+	int numFil, numCol;
+} matInt;
+
+typedef matInt* matIntRef;
+
+matIntRef crearMatInt(int numFil, int numCol, intRef errNum);
+int liberarMatInt(matIntRef mat);
+int leerMatInt(matIntRef m, charRef c);
+int mostrarMatInt(matIntRef m, charRef c);
+matIntRef sumarMatInt(matIntRef a, matIntRef b, intRef errNum);
+
+
+#endif
